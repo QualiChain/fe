19f16f6082a5ea08153faf166dc7261ee54f4425
Miquel Milà Prat <miquel.mila@atos.net>
2019-11-19 13:12:18 +0100

    add some constants into the environments files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: true,
-  authUrl : 'https://localhost:4000/auth'
+  authUrl : 'https://localhost:4000/auth',
+  smartBadgesManagementUrl : 'https://localhost:4000/smartBadgesManagement',
+  verificationAndEquivalenceUrl : 'https://localhost:4000/verificationAndEquivalence',
+  profileUrl : 'https://localhost:4000/profile',
+  educationManagementUrl : 'https://localhost:4000/educationManagement',
+  competencyManagementUrl : 'https://localhost:4000/competencyManagement'  
 };
