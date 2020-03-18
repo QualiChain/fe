9a14613ceb4cb9c5217a3cbdadfa1cceff302c8e
Miquel Milà Prat <miquel.mila@atos.net>
2020-03-18 07:18:12 +0100

    add url into the environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   verificationAndEquivalenceUrl : 'https://localhost:4000/verificationAndEquivalence',
   profileUrl : 'https://localhost:4000/profile',
   educationManagementUrl : 'https://localhost:4000/educationManagement',
-  competencyManagementUrl : 'https://localhost:4000/competencyManagement'  
+  competencyManagementUrl : 'https://localhost:4000/competencyManagement',
+  cvUrl : 'https://localhost:4000/cvs'
 };
