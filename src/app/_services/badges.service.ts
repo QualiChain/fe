af07d5fae2802861adb20d7c7242e37ccc27c801
Miquel Milà Prat <miquel.mila@atos.net>
2020-05-12 19:20:17 +0200

    connect to badges API

---
+++ src/app/_services/badges.service.ts
@@ -0,0 +1,87 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from './../../environments/environment';
+import { throwError } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
+import User from '../_models/user';
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BadgesService {
+
+  
+  private uriBadges = environment.badgesUrl;
+  private uriUser = environment.userUrl;
+
+  constructor(private http: HttpClient) { }
+
+  
+  getBadges() {
+    return this.http.get(`${this.uriBadges}`).
+    pipe(
+       map((data: [any]) => {
+         return data;
+       }), catchError( error => {
+         return throwError( 'Something went wrong!' );
+       })
+    )
+  }
+
+    getBadge(badgeId: Number) {
+      return this.http.get(`${this.uriBadges}/${badgeId}`).
+      pipe(
+         map((data: any) => {
+           return data;
+         }), catchError( error => {
+           return throwError( 'Something went wrong!' );
+         })
+      )
+    }
+
+  addBadge(obj: Object) {
+    return this.http.post(`${this.uriBadges}`, obj).
+    pipe(
+       map((data: any) => {
+         return data;
+       }), catchError( error => {
+         return throwError( 'Something went wrong!' );
+       })
+    )
+  }
+
+  addBadgeToUser(obj: Object) {
+    return this.http.post(`${this.uriUser}/badges`, obj).
+    pipe(
+       map((data: any) => {
+         return data;
+       }), catchError( error => {
+         return throwError( 'Something went wrong!' );
+       })
+    )
+  }
+ 
+  getBadgesByUser(userId: Number) {
+    return this.http.get(`${this.uriUser}/badges?userid=${userId}`).
+    pipe(
+       map((data: any) => {
+         return data;
+       }), catchError( error => {
+         return throwError( 'Something went wrong!' );
+       })
+    )
+  }
+ 
+  deleteBadgeOfUser(badgeId: Number) {
+    return this.http.delete(`${this.uriUser}/badges/${badgeId}`)    
+    .pipe(
+       map((data: any) => {
+         return data;
+       }), catchError( error => {
+         return throwError( 'Something went wrong!' );
+       })
+    )
+  }  
+
+}
