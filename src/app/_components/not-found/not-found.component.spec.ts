4d4d59ae14558896b3658b36b9a980d7d890c768
Miquel Milà Prat <miquel.mila@atos.net>
2020-05-25 19:29:29 +0200

    update to solve kamin errors

--- src/app/_components/not-found/not-found.component.spec.ts
+++ src/app/_components/not-found/not-found.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { NotFoundComponent } from './not-found.component';
+import { TranslateModule } from "@ngx-translate/core";
 
 describe('NotFoundComponent', () => {
   let component: NotFoundComponent;
@@ -8,7 +9,10 @@ describe('NotFoundComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ NotFoundComponent ]
+      declarations: [ NotFoundComponent ],
+      imports: [
+        TranslateModule.forRoot()
+        ]
     })
     .compileComponents();
   }));
