fa69cbcc87786243dbc812580564057c1300014e
Miquel Milà Prat <miquel.mila@atos.net>
2020-06-05 10:28:55 +0200

    add new end point CV and jobs API

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -12,7 +12,7 @@ export const environment = {
   authUrl : 'http://qualichain.epu.ntua.gr:5000/auth',
   usersUrl : 'http://qualichain.epu.ntua.gr:5000/users',//'http://localhost:8000/profiles',
   userUrl : 'http://qualichain.epu.ntua.gr:5000/user',
-  jobsUrl : 'http://localhost:8000/jobs',//'http://qualichain.epu.ntua.gr:5000/jobs',  
+  jobsUrl : 'http://knowledgebizvpn.ddns.net:8000/jobs',//'http://qualichain.epu.ntua.gr:5000/jobs',  
   skillsUrl : 'http://qualichain.epu.ntua.gr:5000/skills',
   notificationsURL : 'http://qualichain.epu.ntua.gr:5000/notifications',
   smartBadgesManagementUrl : 'https://localhost:4000/smartBadgesManagement',
@@ -20,8 +20,8 @@ export const environment = {
   profileUrl : 'https://localhost:4000/profile',
   educationManagementUrl : 'https://localhost:4000/educationManagement',
   competencyManagementUrl : 'https://localhost:4000/competencyManagement',
-  cvUrl : 'http://localhost:8000/cv',//'http://qualichain.epu.ntua.gr:5000/CV',
-  jobmatchingUrl : 'http://localhost:8000/jobs',
+  cvUrl : 'http://knowledgebizvpn.ddns.net:8000/cv',//'http://qualichain.epu.ntua.gr:5000/CV',
+  jobmatchingUrl : 'http://knowledgebizvpn.ddns.net:8000/jobs',//'http://localhost:8000/jobs',
   jobpostGet : 'https://localhost:8000/jobget',  
   jobpostUrl : 'https://localhost:8000/jobs'
 };
