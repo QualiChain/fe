77c9409d476f9b6087dfbed18b5471d3f0bc0bbe
Miquel Milà Prat <miquel.mila@atos.net>
2020-06-08 16:11:36 +0200

    new spinner component

---
+++ src/app/_components/qc-spinner/qc-spinner.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-qc-spinner',
+  templateUrl: './qc-spinner.component.html',
+  styleUrls: ['./qc-spinner.component.css']
+})
+export class QcSpinnerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
