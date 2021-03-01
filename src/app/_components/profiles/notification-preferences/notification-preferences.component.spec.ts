5b772dde18808c4842b5858586c2b64910bfe491
Miquel Milà Prat <miquel.mila@atos.net>
2021-03-01 09:42:36 +0100

    internal_reallocation_availability only for employees

--- src/app/_components/profiles/notification-preferences/notification-preferences.component.spec.ts
+++ src/app/_components/profiles/notification-preferences/notification-preferences.component.spec.ts
@@ -14,6 +14,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { AccessDeniedComponent } from '../../../_components/utils/access-denied/access-denied.component';
 import { QCStorageService } from '../../../_services/QC_storage.services';
+import { AppComponent } from '../../../app.component';
 
 describe('NotificationPreferencesComponent', () => {
   let component: NotificationPreferencesComponent;
@@ -38,7 +39,7 @@ describe('NotificationPreferencesComponent', () => {
         HttpClientTestingModule,
         MatAutocompleteModule
       ],
-      providers: [QCStorageService]
+      providers: [AppComponent, QCStorageService]
     })
     .compileComponents();
   }));
