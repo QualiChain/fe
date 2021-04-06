54ba316a4e8a1f3932b5855ec358828c696c84e2
Miquel Milà Prat <miquel.mila@atos.net>
2021-04-06 09:56:15 +0200

    add verification functionality into the smart badge modal window

--- src/app/_components/utils/qc-smart-badges-list-by-user/qc-smart-badges-list-by-user.component.ts
+++ src/app/_components/utils/qc-smart-badges-list-by-user/qc-smart-badges-list-by-user.component.ts
@@ -3,6 +3,7 @@ import { BadgesService } from '../../../_services/badges.service';
 import { SimpleChanges } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { awardDialog_modal } from '../../../_components/award-smart-badge/award-smart-badge.component';
+import { OUService } from '../../../_services/ou.service'
 
 @Component({
   selector: 'app-qc-smart-badges-list-by-user',
@@ -17,17 +18,29 @@ export class QcSmartBadgesListByUserComponent implements OnInit {
   
   lodingspinnerid: boolean = true;
   aqcuired_badges_by_user: any[] = [];
+
+  errorMessage: string = "";
+  showErrorMessage: boolean = false;
+  loadingLoginSpinner: boolean = false;
+  validationSuccess: boolean = false;
+
   constructor(
     private bs: BadgesService,
     public awardDialog: MatDialog,
+    private ous: OUService
   ) { }
+
+  intialerts() {
+    this.errorMessage = "";
+    this.showErrorMessage = false;
+    this.loadingLoginSpinner = false;
+    this.validationSuccess = false;
+  }
 
   loadUserBadges() {
     this.lodingspinnerid = true;
     this.bs.getBadgesByUser(+this.userId).subscribe(
       badgesByUser => {
-  
-        //console.log(badgesByUser);
         this.aqcuired_badges_by_user = badgesByUser;
         this.lodingspinnerid = false;
         
@@ -46,6 +59,81 @@ export class QcSmartBadgesListByUserComponent implements OnInit {
       this.loadUserBadges();
       
     }
+
+  }
+
+  verifySmartBadge(smartBadgeData) {
+    this.loadingLoginSpinner = true;
+    this.validationSuccess = false;
+    //console.log("verifySmartBadge");
+    //console.log("data In:");
+    //console.log(smartBadgeData);
+
+    let dataBadgetStored = {
+      "badge": smartBadgeData.badge.oubadge,
+      "metadata": smartBadgeData.ou_metadata
+    };
+
+    //console.log("dataBadgetStored");
+    //console.log(dataBadgetStored); 
+    
+    let recipient = smartBadgeData.oubadge_user.recipient;
+    let signature = smartBadgeData.oubadge_user.signature;
+
+    //console.log("recipient");
+    //console.log(recipient);     
+    //console.log("signature");
+    //console.log(signature);     
+    //console.log("this.userId");
+    //console.log(this.userId);
+
+    let dataToPost = {
+      "@context": dataBadgetStored.badge['@context'],
+      "type":"BadgeClass",
+      "name": dataBadgetStored.badge.name,
+      "description": dataBadgetStored.badge.description,
+      "image":  dataBadgetStored.badge.image,
+      "version":  dataBadgetStored.badge.version,
+      "criteria":  dataBadgetStored.badge.criteria,
+      "issuer": dataBadgetStored.badge.issuer,
+      "Assertion": {
+        "type": "Assertion",
+        "recipient": recipient
+      },
+      "issuedOn": + new Date(),
+      "verification": {
+        "type": "MerQLVerification2020" 
+        },
+      "signature": signature
+    };
+
+    //console.log("--data to post to validate signature---");
+    //console.log(dataToPost);
+    this.ous
+          .verifySmartBadgeV2(dataToPost).subscribe(
+          res => {
+
+            this.loadingLoginSpinner = false;
+            console.log("smart badget verification finished, status:"+res);
+            if (res) {
+              this.validationSuccess = true;
+              this.showErrorMessage = false;
+            }
+            else {
+              this.errorMessage = "The validation cannot be done!!!";
+              this.validationSuccess = false;
+              this.showErrorMessage = true;
+              
+            }
+          },
+          error => {
+            console.log(error);
+            this.loadingLoginSpinner = false;
+            this.errorMessage = error.message;
+            this.validationSuccess = false;
+            this.showErrorMessage = true;
+          }
+        );
 
   }
 
